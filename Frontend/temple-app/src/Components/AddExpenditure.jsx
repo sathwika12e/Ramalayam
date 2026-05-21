d1c9b561ad07c994f3375f8ade5ec4408f1abf5c
sathwika12e <KOTASATHWIKA0@GMAIL.COM>
2026-05-21 11:25:37 +0530

    workedd on csspart

--- Frontend/temple-app/src/Components/AddExpenditure.jsx
+++ Frontend/temple-app/src/Components/AddExpenditure.jsx
@@ -23,16 +23,16 @@ function AddExpenditure(){
     return(
         <div>
             <h1>from add expenditure</h1>
-            <div >
-                <select onChange={(e)=>{setCommittee(e.target.value),console.log(e.target.value)}}  value={committee}>
-                        <option value="">Select Committee Member</option>
+            <div style={{marginTop:"10%",display:"flex",flexDirection:"column",width:"60%",margin:"auto",justifyContent:"center",}}> 
+                <select  onChange={(e)=>{setCommittee(e.target.value),console.log(e.target.value)}}  value={committee}>
+                        <option value="">Select a committee member</option>
                      <option value="vemiReddy srinivasulreddy & Bade VeeraraghavaReddy"> vemiReddy srinivasulreddy & Bade VeeraraghavaReddy</option>
                       <option value="Munagala Papireddy & Janapala MalyadriReddy">Munagala Papireddy & Janapala MalyadriReddy</option>
-                      <option value="Kota Ramireddy & Gunupati Prabhakarreddy">Kota Ramireddy & Gunupati Prabhakarreddy</option>
+                      <option  value="Kota Ramireddy & Gunupati Prabhakarreddy">Kota Ramireddy & Gunupati Prabhakarreddy</option>
                 </select>   <br></br><br></br>
                 <input placeholder="expensetype" ref={typeRef} type="text"/><br></br><br></br>
                 <input placeholder="amount" ref={amtRef} type="number"/><br></br><br></br>
-                <button onClick={submitfun}>Submit</button>
+                <button style={{width:"30%",marginLeft:"35%",backgroundColor:"blue",color:"white",borderRadius:"2vh"}} onClick={submitfun}>Submit</button>
             </div>
         </div>
     )
